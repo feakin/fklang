7fe49268461f79d3dcd2d302e7498df19d91f8fe
Phodal Huang <h@phodal.com>
2022-10-10 22:16:37 +0800

    feat: make tests works

--- fkl_cli/src/e2e.rs
+++ fkl_cli/src/e2e.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+  use std::fs;
   use std::path::PathBuf;
   use crate::code_gen_exec;
 
@@ -14,5 +15,34 @@ mod tests {
     input_path.push(format!("spring.fkl"));
 
     code_gen_exec::code_gen_exec(Some(&input_path), Some(&"HelloGot".to_string()), &base_path);
+
+    let controller = "test_data/spring/src/main/java/com/feakin/demo/rest/HelloController.java";
+    let output = fs::read_to_string(controller).expect("Something went wrong reading the file");
+    assert!(output.contains(r#"
+    @GetMapping("/hello")
+    public String gotHello() {
+
+    }"#));
+
+    reset_test(controller);
+  }
+
+  fn reset_test(controller: &str) {
+// reset test
+    fs::write(controller, r#"package com.feakin.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
+"#).unwrap();
   }
 }
