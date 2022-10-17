2d463c8c3e94ffa0d59fb0f98ae3881b1293de64
Phodal Huang <h@phodal.com>
2022-10-17 22:06:28 +0800

    test: add test for endpoint runner

--- fkl_cli/src/builtin/endpoint_runner.rs
+++ fkl_cli/src/builtin/endpoint_runner.rs
@@ -1,4 +1,8 @@
-use fkl_parser::mir::HttpEndpoint;
+use std::collections::HashMap;
+
+use log::info;
+
+use fkl_parser::mir::{HttpEndpoint, HttpMethod};
 
 pub struct EndpointRunner {
   endpoint: HttpEndpoint,
@@ -12,10 +16,65 @@ impl EndpointRunner {
   }
 
   pub fn send_request(&self) -> Result<(), ()> {
-    let client = reqwest::Client::new();
-    // match self.endpoint.method {
-    //
-    // }
+    let client = reqwest::blocking::Client::new();
+    let req = self.request_to_hashmap();
+
+    match self.endpoint.method {
+      HttpMethod::GET => {
+        client.get(&self.endpoint.path)
+          .send()
+          .expect("Failed to send request");
+      }
+      HttpMethod::POST => {
+        client.post(&self.endpoint.path)
+          .send()
+          .expect("Failed to send request");
+      }
+      HttpMethod::PUT => {
+        client.put(&self.endpoint.path)
+          .send()
+          .expect("Failed to send request");
+      }
+      HttpMethod::DELETE => {
+        client.delete(&self.endpoint.path)
+          .send()
+          .expect("Failed to send request");
+      }
+      HttpMethod::PATCH => {
+        client.patch(&self.endpoint.path)
+          .send()
+          .expect("Failed to send request");
+      }
+      _ => {
+        info!("Unsupported method: {:?}", self.endpoint.method);
+      }
+    }
+
     Ok(())
+  }
+  fn request_to_hashmap(&self) -> HashMap<String, String> {
+    let mut map = HashMap::new();
+    // todo: convert request
+    map
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  #[should_panic]
+  fn test_request_to_hashmap() {
+    let endpoint = HttpEndpoint {
+      name: "".to_string(),
+      method: HttpMethod::GET,
+      path: "/test".to_string(),
+      request: None,
+      response: None,
+      description: "".to_string()
+    };
+    let runner = EndpointRunner::new(endpoint);
+    let map = runner.send_request();
   }
 }
