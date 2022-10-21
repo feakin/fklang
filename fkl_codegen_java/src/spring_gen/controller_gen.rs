6abf7c85a97c114f9930384c8c57fa4d5e116f1d
Phodal Huang <h@phodal.com>
2022-10-21 21:18:01 +0800

    refactor: add controller

---
+++ fkl_codegen_java/src/spring_gen/controller_gen.rs
@@ -0,0 +1,15 @@
+pub struct ControllerGen {}
+
+impl ControllerGen {
+  pub fn new() -> ControllerGen {
+    ControllerGen {}
+  }
+
+  pub fn controller_content() {
+
+  }
+
+  pub fn controller_file() {
+
+  }
+}
--- fkl_codegen_java/src/spring_gen/mod.rs
+++ fkl_codegen_java/src/spring_gen/mod.rs
@@ -1,3 +1,4 @@
 pub mod spring_code_gen;
 pub mod entity_gen;
 pub mod jpa_gen;
+pub mod controller_gen;
