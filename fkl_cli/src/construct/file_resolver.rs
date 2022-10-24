29ae6033004327e8958232c545351749aa381c6f
Phodal Huang <h@phodal.com>
2022-10-24 21:19:01 +0800

    refactor: add parse for imports

--- fkl_cli/src/construct/file_resolver.rs
+++ fkl_cli/src/construct/file_resolver.rs
@@ -5,6 +5,8 @@ use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 use crate::code_meta::{CodeFile, CodeLanguage};
+use crate::construct::code_construct::CodeConstruct;
+use crate::construct::java_construct::JavaConstruct;
 
 // inspired by [Solang](https://github.com/hyperledger-labs/solang)
 pub struct FileResolver {
@@ -12,12 +14,13 @@ pub struct FileResolver {
   files: HashMap<PathBuf, ResolvedFile>,
 }
 
+#[allow(dead_code)]
 #[derive(Clone, Debug)]
 pub struct ResolvedFile {
   content: Arc<str>,
-  import_paths: Vec<(String, PathBuf)>,
+  imports: Vec<String>,
   language: CodeLanguage,
-  meta: CodeFile,
+  meta: Option<CodeFile>,
   path: PathBuf,
 }
 
@@ -31,6 +34,22 @@ impl Default for FileResolver {
 }
 
 impl FileResolver {
+  fn load_dir(&mut self, path: &Path) {
+    for entry in walkdir::WalkDir::new(path) {
+      let entry = entry.unwrap();
+      if !entry.file_type().is_file() {
+        continue;
+      }
+
+      let path = entry.path();
+      if let None = path.extension() {
+        continue;
+      }
+
+      self.load_file(path).unwrap();
+    }
+  }
+
   fn load_file(&mut self, path: &Path) -> Result<(), String> {
     if self.cached_paths.get(path).is_some() {
       return Ok(());
@@ -50,13 +69,31 @@ impl FileResolver {
     let pos = self.files.len();
     self.cached_paths.insert(path.to_path_buf(), pos);
 
+    let mut imports: Vec<String> = vec![];
+    let meta = match path.extension() {
+      Some(ext) => {
+        match ext.to_str().unwrap_or("") {
+          "java" => {
+            let code_file = JavaConstruct::parse(&content);
+            imports = code_file.imports.clone();
+            Some(code_file)
+          }
+          &_ => {
+            None
+          }
+        }
+      }
+      None => return Err(format!("{}: no file extension", path.display()))
+    };
+
     let resolved_file = ResolvedFile {
       content: Arc::from(content),
-      import_paths: Default::default(),
+      imports,
       language: CodeLanguage::Java,
-      meta: Default::default(),
+      meta,
       path: path.to_path_buf(),
     };
+
     self.files.insert(path.to_path_buf(), resolved_file);
 
     Ok(())
@@ -78,5 +115,25 @@ mod tests {
 
     assert_eq!(resolver.files.len(), 1);
     assert_eq!(resolver.cached_paths.len(), 1);
+
+    let file = resolver.files.get(&d).unwrap();
+    assert_eq!(file.language, CodeLanguage::Java);
+    assert_eq!(file.meta.is_some(), true);
+    assert_eq!(file.imports.len(), 2);
+    assert_eq!(file.imports[0], "org.springframework.boot.SpringApplication");
+    assert_eq!(file.meta.as_ref().unwrap().classes[0].name, "DemoApplication");
+  }
+
+  #[test]
+  fn test_load_dir() {
+    let mut resolver = FileResolver::default();
+
+    let mut d: PathBuf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("test_data/spring/src/main/java/com/feakin/demo");
+
+    resolver.load_dir(&d);
+
+    assert_eq!(resolver.files.len(), 2);
+    assert_eq!(resolver.cached_paths.len(), 2);
   }
 }
