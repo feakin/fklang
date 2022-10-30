2c58b2b94a90d1081c1bddbb99d81498c89513ff
Phodal Huang <h@phodal.com>
2022-10-30 23:35:27 +0800

    refactor: reimplementation serde for fakevalue #9

--- fkl_cli/src/mock/mock_type.rs
+++ fkl_cli/src/mock/mock_type.rs
@@ -1,8 +1,9 @@
 use indexmap::IndexMap;
-use serde::Deserialize;
+use serde::{Deserialize, Serializer};
+use serde::ser::{SerializeMap, SerializeSeq};
 use serde::Serialize;
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[derive(Deserialize, Debug, Clone, PartialEq)]
 pub enum FakeValue {
   Null,
   // Optional(Box<MockType>),
@@ -81,19 +82,66 @@ impl FakeValue {
   }
 }
 
+impl Serialize for FakeValue {
+  fn serialize<S>(&self, s: S) -> Result<S::Ok, S::Error> where S: Serializer {
+    match self {
+      FakeValue::Null => s.serialize_none(),
+      FakeValue::Unknown(str) => s.serialize_str(str),
+      FakeValue::String(str) => s.serialize_str(str),
+      FakeValue::Integer(i) => s.serialize_i64(*i),
+      FakeValue::Float(f) => s.serialize_f64(*f),
+      FakeValue::Boolean(b) => s.serialize_bool(*b),
+      FakeValue::Array(a) => {
+        let mut seq = s.serialize_seq(Some(a.len()))?;
+        for item in a {
+          seq.serialize_element(item)?;
+        }
+        seq.end()
+      },
+      FakeValue::Map(m) => {
+        let mut map = s.serialize_map(Some(m.len()))?;
+        for (key, value) in m {
+          map.serialize_entry(key, value)?;
+        }
+        map.end()
+      },
+      FakeValue::Date(d) => s.serialize_str(d),
+      FakeValue::DateTime(dt) => s.serialize_str(dt),
+      FakeValue::Timestamp(t) => s.serialize_i64(*t),
+      FakeValue::Uuid(u) => s.serialize_str(u),
+    }
+  }
+}
+
 #[cfg(test)]
 mod tests {
-  use std::collections::HashMap;
+  use indexmap::IndexMap;
   use crate::mock::mock_type::FakeValue;
 
   #[test]
   fn test_serde_json() {
-    let mut map: HashMap<String, FakeValue> = HashMap::new();
+    let mut map: IndexMap<String, FakeValue> = IndexMap::new();
     map.insert("a".to_string(), FakeValue::String("b".to_string()));
     map.insert("c".to_string(), FakeValue::Integer(1));
 
     let s = serde_json::to_string(&map).unwrap();
 
-    assert_eq!(s, r#"{"a":{"String":"b"},"c":{"Integer":1}}"#);
+    assert_eq!(s, r#"{"a":"b","c":1}"#);
+  }
+
+  #[test]
+  fn nested_fake_value() {
+    let mut map: IndexMap<String, FakeValue> = IndexMap::new();
+    map.insert("a".to_string(), FakeValue::String("b".to_string()));
+    map.insert("c".to_string(), FakeValue::Integer(1));
+
+    let mut map2: IndexMap<String, FakeValue> = IndexMap::new();
+    map2.insert("d".to_string(), FakeValue::String("e".to_string()));
+    map2.insert("f".to_string(), FakeValue::Integer(2));
+    map2.insert("g".to_string(), FakeValue::Map(map));
+
+    let s = serde_json::to_string(&map2).unwrap();
+
+    assert_eq!(s, r#"{"d":"e","f":2,"g":{"a":"b","c":1}}"#);
   }
 }
