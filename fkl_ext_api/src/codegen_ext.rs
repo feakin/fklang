4a1ebac8dd89ea7f23f7446cfbc771cacddd4fc9
Phodal Huang <h@phodal.com>
2022-11-04 09:57:50 +0800

    feat: init runner context #10

--- fkl_ext_api/src/codegen_ext.rs
+++
@@ -1,9 +0,0 @@
-pub struct CodegenExt {
-  pub name: String,
-  pub parser: String,
-}
-
-pub trait CodegenTrait {
-  fn name(&self) -> String;
-  fn parser(&self) -> String;
-}
--- fkl_ext_api/src/source_set_loader.rs
+++
@@ -1,4 +0,0 @@
-pub trait SourceSetLoaderTrait {
-  fn name() -> String;
-  fn load(path: String) -> String;
-}
