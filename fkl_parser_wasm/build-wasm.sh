d26bdb8962d663f86d24a49e2c8bb61651d11427
Phodal Huang <h@phodal.com>
2022-11-09 23:03:55 +0800

    refactor: align all package names

--- fkl_parser_wasm/build-wasm.sh
+++ fkl_parser_wasm/build-wasm.sh
@@ -5,9 +5,9 @@ RUSTFLAGS=""
 rm -rf pkg-*
 
 wasm-pack build --target web --out-dir pkg-web --out-name fkl .
-sed -i.old 's/"name": "fkl-wasm"/"name": "@feakin\/fkl-wasm-web"/' pkg-web/package.json
+sed -i.old 's/"name": "fkl_wasm"/"name": "@feakin\/fkl-wasm-web"/' pkg-web/package.json
 
 wasm-pack build --target nodejs --out-dir pkg-node --out-name fkl .
-sed -i.old 's/"name": "fkl-wasm"/"name": "@feakin/fkl-wasm-node"/' pkg-node/package.json
+sed -i.old 's/"name": "fkl_wasm"/"name": "@feakin/fkl-wasm-node"/' pkg-node/package.json
 
 brotli -f pkg-web/*.wasm
